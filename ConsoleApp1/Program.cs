46058f63af6a67454c6f44e372ddf3d3febeb373
Чеканова Алиса Юрьевна <gr613_cheayur@ttit.local>
2023-04-22 12:34:42 +0700

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберете нужную функцию \n 1. Наибольшее из двух числе");
+            //int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберете нужную функцию \n 2. Рандомный массив из 10 чисел");
+            //int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберете нужную функцию \n 3. Сумма чисел");
+            // int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберете нужную функцию \n 4. Разность чисел");
+            //int d = int.Parse(Console.ReadLine());
+
+            string vbr = Console.ReadLine();
+
+            switch (vbr)
+            {
+                case "1":
+                    
+                    break;
+                case "2":
+                    Console.WriteLine();
+                    break;
+                case "+":
+                    Console.WriteLine();
+                    break;
+                case "-":
+                    Console.WriteLine();
+                    break;
+            }
+        }
+       
+
+
+
+
+
+    }
+}
+
